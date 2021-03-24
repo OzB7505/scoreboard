da89a56332b8f8001b490fca6db155bb82ecad98
OzB7505 <obal7505@richmondhillhs.org>
2021-03-24 13:52:22 +0000

    finished

--- src/App.js
+++ src/App.js
@@ -7,34 +7,65 @@ class App extends React.Component {
     super(props);
     this.state = {
       playerAScore: 0,
+      playerBScore: 0,
       winningScore: 0,
-      winner: "Player A",
+      winner: "?",
     }
   }  
 
   handlePlayerAClick = () => {
     this.setState(state => {
       state.playerAScore += 1
-      return state
+      if (this.state.playerAScore > this.state.playerBScore){
+          state.winner = "A"
+          state.winningScore = state.playerAScore
+          return state
+
+      } else if (this.state.playerAScore === this.state.playerBScore) {
+          state.winner="?"
+          state.winningScore = "Tie"
+          return state
+      } else {
+          return state
+      }
+    });
+  }
+  handlePlayerBClick = () => {
+    this.setState(state => {
+      state.playerBScore += 1
+      if (this.state.playerBScore > this.state.playerAScore){
+          state.winner = "B"
+          state.winningScore = state.playerBScore
+          return state
+
+      } else if (this.state.playerAScore === this.state.playerBScore) {
+          state.winner="?"
+          state.winningScore = "tie"
+          return state
+      } else {
+          return state
+      }
     });
   }
 
   reset = () => {
     this.setState({
       playerAScore: 0,
+      playerBScore: 0,
       winningScore: 0,
-      winner: "Player A",
+      winner: "?" ,
     });
   }
-
+  
   render() {
     return (
       <div className="App">
         <div className="banner">SCOREBOARD</div>
-        <h1 className="Winner">Player ## is in the lead!</h1>
-        <h3>The winner has ## points</h3>
+        <h1 className="Winner">Player {this.state.winner} is in the lead!</h1>
+        <h3>The winner has {this.state.winningScore} points</h3>
         <div className="Players">
           <Player id={0} name={"A"} score={this.state.playerAScore} clickHandler={this.handlePlayerAClick}/>
+          <Player id={1} name={"B"} score={this.state.playerBScore} clickHandler={this.handlePlayerBClick}/>
         </div>
         <button className="ResetButton" onClick={this.reset}>Reset</button>
       </div>
